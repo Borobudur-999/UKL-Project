a46fa8471233f37aad688aa4038074ece7ec045a
Borobudur-999 <anwarilwasis80@gmail.com>
2025-12-02 09:45:11 +0700

    smetlsystem

--- Assets/SmeltActive.cs
+++ Assets/SmeltActive.cs
@@ -7,6 +7,8 @@ public class SmeltActive : MonoBehaviour
     public GameObject _panel;
     public float _y;
 
+    private bool isOpen = false;
+
     void Start()
     {
         _button.onClick.AddListener(OpenSmelt);
@@ -14,7 +16,22 @@ public class SmeltActive : MonoBehaviour
 
     void OpenSmelt()
     {
-        Debug.Log("open");
+        // cek apakah panel sudah terbuka
+        if (isOpen) return;
+
+        isOpen = true;                     // tandai sudah membuka panel
+        _button.interactable = false;      // matikan tombol biar tidak bisa dipencet lagi
+
+        // buka panel
+        // Debug.Log("open");
         _panel.transform.Translate(0, _y, 0);
+    }
+
+    public void CloseSmelt()
+    {
+        isOpen = false;                    // panel sudah ditutup
+        _button.interactable = true;       // tombol aktif kembali
+
+        _panel.transform.Translate(0, -_y, 0);
     }
 }
