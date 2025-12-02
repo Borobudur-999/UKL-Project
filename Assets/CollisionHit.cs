365a447852bbd3f296248914eafd3f10de029b7d
Borobudur-999 <anwarilwasis80@gmail.com>
2025-12-02 08:00:44 +0700

    up

--- Assets/CollisionHit.cs
+++ Assets/CollisionHit.cs
@@ -3,13 +3,11 @@ using UnityEngine;
 public class CollisionHit : MonoBehaviour
 {
     public float _boundForce = 10;
-    public GameObject _block;
     private Rigidbody2D _rb;
 
     void Start()
     {
         _rb = FindAnyObjectByType<Rigidbody2D>();
-        _block.SetActive(false);
     }
     
     private void OnCollisionEnter2D(Collision2D collision)
@@ -17,7 +15,6 @@ public class CollisionHit : MonoBehaviour
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             Destroy(collision.gameObject);
-            _block.SetActive(true);
 
             _rb.velocity = new Vector2(_rb.velocity.x, 0);
 
