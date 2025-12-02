f465b1d5695505c5cffe8a9678eed3121a326614
Xavioletz <rasheesax@gmail.com>
2025-12-02 07:43:09 +0700

    Up block

--- Assets/CollisionHit.cs
+++ Assets/CollisionHit.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class CollisionHit : MonoBehaviour
 {
     public float _boundForce = 10;
+    public GameObject _block;
     private Rigidbody2D _rb;
 
     void Start()
     {
         _rb = FindAnyObjectByType<Rigidbody2D>();
+        _block.SetActive(false);
     }
     
     private void OnCollisionEnter2D(Collision2D collision)
@@ -15,6 +17,7 @@ public class CollisionHit : MonoBehaviour
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             Destroy(collision.gameObject);
+            _block.SetActive(true);
 
             _rb.velocity = new Vector2(_rb.velocity.x, 0);
 
