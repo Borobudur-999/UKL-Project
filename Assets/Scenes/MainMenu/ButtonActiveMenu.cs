034b7af1d2bd9f38f4f01aa22b00275c3193fc7e
Borobudur-999 <anwarilwasis80@gmail.com>
2025-12-04 10:54:00 +0700

    update Mianmenu

---
+++ Assets/Scenes/MainMenu/ButtonActiveMenu.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ButtonActiveMenu : MonoBehaviour
+{
+    public void OpenCredit()
+    {
+        
+    }
+}
---
+++ Assets/ValueScroll.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValueScroll : MonoBehaviour
+{
+    public Scrollbar _scroll;
+
+    public void Update()
+    {
+        
+    }
+}
