b4c8b1126d1e2c159121fcad6e3a749b571d2ab6
Borobudur-999 <anwarilwasis80@gmail.com>
2025-12-10 11:01:31 +0700

    Final Update

--- Assets/Scenes/MainMenu/ButtonActiveMenu.cs
+++ Assets/Scenes/MainMenu/ButtonActiveMenu.cs
@@ -3,9 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class ButtonActiveMenu : MonoBehaviour
 {
+    public GameObject _p;
     public string scene;
+    public void Start()
+    {
+        _p.SetActive(false);
+    }
     public void Gameplay()
     {
         SceneManager.LoadScene(scene);
+    }
+
+    public void Open()
+    {
+        _p.SetActive(true);
+    }
+    public void Opepn()
+    {
+        _p.SetActive(false);
     }
 }
